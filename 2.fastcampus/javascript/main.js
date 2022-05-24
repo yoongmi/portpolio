bd13e3a75587a98d5cc48f516582318164660796
yoonmi12 <xduathx@naver.com>
2022-05-24 16:35:23 +0900

    javascript 시작

---
+++ 2.fastcampus/javascript/main.js
@@ -0,0 +1,14 @@
+import getType from './getType'
+
+console.log(typeof 123)
+console.log(typeof true)
+console.log(typeof undefined)
+console.log(typeof null)
+console.log(typeof [])
+console.log(typeof {})
+
+console.log(getType(123))
+console.log(getType(false))
+console.log(getType(null))
+console.log(getType({}))
+console.log(getType([]))
