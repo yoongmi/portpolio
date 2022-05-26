a777164933f3f3a147dafc639035e0711f01e944
yoonmi12 <xduathx@naver.com>
2022-05-26 16:55:37 +0900

    화살표함수 완료

--- 2.fastcampus/javascript/main.js
+++ 2.fastcampus/javascript/main.js
@@ -12,3 +12,22 @@ console.log(getType(false))
 console.log(getType(null))
 console.log(getType({}))
 console.log(getType([]))
+
+
+// 화살표 함수
+// () => {} vs function() {}
+
+const double = function(x){
+    return x+2
+}
+console.log('double:',double(7))
+
+
+const doubleArrow = (x) => x+2
+const doubleArrow2 = x => x+2
+const doubleArrow3 = x => ({name:'hello'}); 
+console.log('doubleArrow',doubleArrow(7),doubleArrow2(7));
+console.log(doubleArrow3(name));
+
+
+// 즉시실행함수
