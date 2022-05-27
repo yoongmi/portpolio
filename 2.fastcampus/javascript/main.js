c631e12596b10915ac1ad46dfa47caad1531a384
yoonmi12 <xduathx@naver.com>
2022-05-27 16:43:44 +0900

    JS함수 공부

--- 2.fastcampus/javascript/main.js
+++ 2.fastcampus/javascript/main.js
@@ -30,4 +30,122 @@ console.log('doubleArrow',doubleArrow(7),doubleArrow2(7));
 console.log(doubleArrow3(name));
 
 
-// 즉시실행함수
+// 즉시실행함수 IIFE
+const a = 5;
+/*
+(function(){
+    console.log(a*2)
+})()
+
+(function(){
+    console.log(a*2)
+}())
+*/
+
+// 타이머 
+/*
+const timer = setInterval(() =>{
+    console.log('HEROPY!')
+},1000)
+
+const title = document.querySelector('h1');
+title.addEventListener('click',() => {
+    clearInterval(timer)
+})
+*/
+
+//콜백함수
+/*
+function timeout(cb){
+    setTimeout(()=>{
+        console.log('hihi')
+        cb()
+    },3000)
+}
+timeout(()=>{
+    console.log('Done!')
+})
+*/
+//prototype
+/*
+const heropy = {
+    firstName: 'Heropy',
+    lastName: 'Park',
+    getFullName: function(){
+        return `${this.firstName} ${this.lastName}`
+    }
+}
+console.log(heropy.getFullName());
+
+const neo = {
+    firstName: 'Neo',
+    lastName: 'Smith',
+    getFullName: function(){
+        return `${this.firstName} ${this.lastName}`
+    }
+}
+console.log(neo.getFullName());
+*/
+/*
+function User(first, last){
+    this.firstName = first
+    this.lastName = last
+}
+User.prototype.getFullName = function(){
+    return `${this.firstName} ${this.lastName}`
+}
+
+const park = new User("Heropy","Park")  //생성자함수
+const amy = new User("amy","Park")
+const neo = new User("neo","Park")
+
+console.log(park.getFullName())
+console.log(amy)
+console.log(neo)
+*/
+/*
+const thisstudy = {
+    name: 'yoonmi',
+    normal : function(){
+        console.log(this.name)
+    },
+    arrow: () => {
+        console.log(this.name)
+    }
+}
+thisstudy.normal()
+thisstudy.arrow()
+
+const any = {
+    name:'Any',
+    normal: heropy.normal,
+    arrow:heropy.arrow
+}
+any.normal()
+any.arrow()
+
+
+const teraphy = {
+    name:"yoon",
+    timeout:function(){
+        setTimeout(()=>{
+            console.log(this.name)
+        },2000)
+    }
+}
+teraphy.timeout();
+*/
+
+class User {
+    constructor(first,last){
+        this.firstName = first
+        this.lastName = last
+    }
+    getFullName(){
+        return `${this.firstName} ${this.lastName}`
+    }
+}
+
+const park = new User("Heropy","Park")  //생성자함수
+
+console.log(park.getFullName())
