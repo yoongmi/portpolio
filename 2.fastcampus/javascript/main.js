8e21d2623bbb4e7c884ce5a9c992474e351c1f4d
yoonmi12 <xduathx@naver.com>
2022-05-30 16:34:53 +0900

    JS 상속, Method

--- 2.fastcampus/javascript/main.js
+++ 2.fastcampus/javascript/main.js
@@ -1,3 +1,41 @@
+
+//HTML 요소 검색/찾기
+const boxEl = document.querySelector('.box');
+
+//HTML 요소에 적용할 수 있는 메소드
+// boxEl.addEventListener();
+
+//인수를 추가 기능
+// boxEl.addEventListener(1,2);
+
+// 핸들러(handler, 실행할함수)
+// boxEl.addEventListener('click',function(){
+// })
+
+//HTML 요소 모두 검색/찾기
+const boxEls = document.querySelectorAll('.box')
+
+//찾은 요소들 반복해서 함수 실행
+//익명 함수를 인수로 추가
+boxEls.forEach(function(){})
+
+//첫 번째 매개변수(boxEl) : 반복중인 요소,
+//두 번째 매개변수(index) : 반복중인 번호
+boxEls.forEach(function(boxEl,index){})
+
+//출력
+boxEls.forEach(function(boxEl,index){
+    boxEl.classList.add(`order-${index}`);
+    console.log(index,boxEl)
+})
+
+// getter 값을 얻는 용도
+console.log(boxEl.textContent);
+
+// setter 값을 지정하는 용도
+boxEl.textContent='하나둣!!!';
+
+
 import getType from './getType'
 
 console.log(typeof 123)
@@ -149,3 +187,38 @@ class User {
 const park = new User("Heropy","Park")  //생성자함수
 
 console.log(park.getFullName())
+
+
+//상속(확장)
+class Vehicle{
+    constructor(name,wheel){
+        this.name = name
+        this.wheel = wheel
+    }
+}
+
+const myVehicle = new Vehicle('운송수단',2)
+console.log(myVehicle)
+
+class Bicycle extends Vehicle{
+    constructor(name,wheel){
+        super(name,wheel)
+    }
+}
+
+const myBicycle = new Bicycle('삼천리',2)
+const daughtersBicycle = new Bicycle('세발',3)
+console.log(myBicycle)
+console.log(daughtersBicycle)
+
+class Car extends Vehicle{
+    constructor(name,wheel,license){
+        super(name,wheel)
+        this.license = license
+    }
+}
+
+const myCar = new Car ('벤츠',4,true)
+const daughtersCar = new Car ('포르쉐',4,false)
+console.log(myCar)
+console.log(daughtersCar)
