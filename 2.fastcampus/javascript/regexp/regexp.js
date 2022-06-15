bda4cea212ae34716efd810d6e3329918797ad99
yoonmi <xduathx@naver.com>
2022-06-15 21:22:20 +0900

    정규표현식 끝

--- 2.fastcampus/javascript/regexp/regexp.js
+++ 2.fastcampus/javascript/regexp/regexp.js
@@ -3,7 +3,9 @@ const str = `
 yoon@gmail.com
 https://www.omdbapi.com/?apikey=7035c60c&s=frozen
 The quick brown fox jumps over the lazy dog.
-aasdffasdf
+aasdffasdfdddd
+http://localhost:1234
+동해물과_백두산이 마르고 닳도록
 `
 
 
@@ -19,3 +21,32 @@ const regexp3 = /the/gi
 console.log(str.match(regexp3))
 
 console.log(str.match(/\.$/gim))  // \(백슬래시) 일반적인 문자로 해석할수있는. 문자끝에 마침표가 있는지.
+
+console.log(str.match(/.^t/gim))
+
+console.log(str.match(/dog|fox/g))
+
+console.log(str.match(/https?/g)) //?는 있을수도 있고 없을수도 있는 
+
+console.log(str.match(/\b\w{2,3}\b/g))
+
+console.log(str.match(/[0-9]{1,}/g))
+console.log(str.match(/[가-힣]{1,}/g))
+
+console.log(str.match(/\bf\w{1,}\b/g))  //숫자 f로 시작되는 단어
+
+const h = `   the hello world     !!
+
+`
+console.log(h.replace(/\s/g,''))
+
+const str2 = `
+010-1234-4567
+yoon@gmail.com
+https://www.omdbapi.com/?apikey=7035c60c&s=frozen
+The quick brown fox jumps over the lazy dog.
+aasdffasdf
+`
+
+console.log(str.match(/(?=>@).{1,}/g))
+console.log(str.match(/.{1,}(?=@)/g))
