ca31ebeac529acf3b50152ab9b1378aef895a167
yoonmi <xduathx@naver.com>
2022-07-02 08:52:18 +0900

    typescript nomad test1

---
+++ 1.nomadcoder/typescript/test/test01.js
@@ -0,0 +1,34 @@
+/* my */
+
+function last<T>(arr: T[]){
+    return arr[arr.length -1];
+}
+function prepend<T>(arr:T[],item:T){
+    const arrayAdd = arr.unshift(item)
+    return arrayAdd
+}
+
+
+
+/* nico */
+// Last
+
+type Last = <T>(items: T[]) => T;
+
+const last: Last = (items) => items[items.length - 1];
+
+const lastItem = last([1, 2, 3, 4, 5]);
+
+console.log(lastItem);
+
+// Prepend
+
+type Prepend = <T>(items: T[], item: T) => T[];
+
+const prepend: Prepend = (items, item) =>  [item, ...items]
+
+const items = [1, 2, 3, 4, 5];
+
+const newItems = prepend(items,0);
+
+console.log(newItems)
